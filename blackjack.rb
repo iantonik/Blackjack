e1f5d30c4778a747b47534995087e7480ebd8553
iantonik <iwona.antonik@gmail.com>
2015-07-07 21:47:14 -0400

    Final Blackjack Game

--- blackjack.rb
+++ blackjack.rb
@@ -1,46 +1,122 @@
-=begin
-Rules & Requirements:
+#Method def start
 
-Blackjack is a card game where you calculate the sum of the values of your cards and try to hit 21, aka "blackjack". Both the player and dealer are dealt two cards to start the game. All face cards are worth whatever numerical value they show. Suit cards are worth 10. Aces can be worth either 11 or 1. Example: if you have a Jack and an Ace, then you have hit "blackjack", as it adds up to 21.
+def initialize_deck
+  # Define cards and playing deck
 
-After being dealt the initial 2 cards, the player goes first and can choose to either "hit" or "stay". Hitting means deal another card. If the player's cards sum up to be greater than 21, the player has "busted" and lost. If the sum is 21, then the player wins. If the sum is less than 21, then the player can choose to "hit" or "stay" again. If the player "hits", then repeat above, but if the player stays, then the player's total value is saved, and the turn moves to the dealer.
+  suits = ['♥', '♦', '♠', '♣']
+  numbers = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
 
-By rule, the dealer must hit until she has at least 17. If the dealer busts, then the player wins. If the dealer, hits 21, then the dealer wins. If, however, the dealer stays, then we compare the sums of the two hands between the player and dealer; higher value wins.
+  $playing_cards = numbers.product(suits)
+  number_of_decks = 0
+  while number_of_decks < 1 || number_of_decks > 5
+    puts "How many decks do you want to play with (1-5)?"
+    number_of_decks = gets.chomp.to_i
+  end
+  $playing_cards *= number_of_decks
+  puts "You are playing with #{$playing_cards.count} cards."
+  $playing_cards.shuffle!
+end
 
-1. Save the player's name, and use it throughout the app.
-2. Ask the player if he wants to play again, rather than just exiting.
-3. Save not just the card value, but also the suit.
-4. Use multiple decks to prevent against card counting players.
+def deal_initial_hand
+  $dealer_hand = []
+  $player_hand = []
+  2.times do
+    $dealer_hand.push $playing_cards.pop
+    $player_hand.push $playing_cards.pop
+  end
+end
 
-Clovers(♣), diamonds (♦), hearts (♥) and spades (♠)
-=end
+#Define card points
 
-# Command line blackjack game.
+def get_card_points card
+  points = card[0].to_i
+  if points == 0
+    if card[0] == 'A'
+      points = 1;
+    else points = 10
+    end
+  end
+  return points
+end
 
-suits = ['♥', '♦', '♠', '♣']
-cards = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+#Calcumlate total
+
+def get_total_points hand
+  sum = 0
+  number_of_aces = 0
+  for card in hand
+    card_pts = get_card_points card
+    if card_pts == 1
+      number_of_aces += 1
+    end
+      sum += card_pts
+  end
+  number_of_aces.times do
+    (sum <= 11) ? sum += 10 : break
+  end
+  return sum
+end
+
+#Method def end
+initialize_deck
+deal_initial_hand
+
+puts "Dealer hand: #{$dealer_hand.first},hidden"
+puts "Player hand: #{$player_hand}"
+
+dealer_total = 0
+player_total = 0
 
 
+stop_playing = false
+player_busted = false
 
-playing_cards = cards.product(suits)
-playing_cards.map!(&:join)
-playing_cards.shuffle!
+while !stop_playing
+  user_choice = " "
+  while user_choice !='H' && user_choice !='S'
+    puts 'Hit (H) or Stay (S)?'
+    user_choice = gets.chomp.upcase
+  end
+  player_total = get_total_points $player_hand
+  if user_choice =='H'
+    $player_hand.push $playing_cards.pop
+    player_total = get_total_points $player_hand
+    puts "Player hand #{$player_hand}"
+    if player_total > 21
+      puts "Bust! You lose."
+      stop_playing = true
+      player_busted = true
+    end
+    if player_total == 21
+      puts "BLACKJACK!!! You win!"
+      stop_playing = true
+    end
+  else
+    break
+  end
+end
 
-puts playing_cards
+if !player_busted
+  stop_playing = false
+  while !stop_playing
+    dealer_total = get_total_points $dealer_hand
+    if dealer_total >= 17
+      stop_playing = true
+      puts "Dealer hand #{$dealer_hand}"
+      puts "Player hand #{$player_hand}"
+      if dealer_total > 21
+        puts "Dealer Busts!"
+      else
+        puts "Dealer total: #{dealer_total}"
+        puts "Player total: #{player_total}"
+      end
+    end
+    $dealer_hand.push $playing_cards.pop
+  end
+end
 
-dealer_hand = []
-player_hand = []
-
-
-
-
-
-# define how cards will be calculated
-
-
-
-
-
-
-
-
+if (player_total > 21 || (dealer_total < 22 && dealer_total >= player_total))
+  puts "Dealer Wins!"
+else
+  puts "Player Wins!"
+end
